c85e109033e81782acb5e593965dae76d9a80716
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2021-12-11 21:36:44 +0100

    Update runDocker.bat

--- Docker/runDocker.bat
+++ Docker/runDocker.bat
@@ -6,7 +6,7 @@ echo Creating working directory: %WORKING_DIR%
 mkdir %WORKING_DIR%
 
 echo Checking for container updates.
-docker pull akalinow/root-fedora31
+docker pull akalinow/root-fedora35
 
 echo Starting container.
 docker run -ti --rm -v %WORKING_DIR%:/scratch -p 8000:8000 akalinow/root-fedora35
